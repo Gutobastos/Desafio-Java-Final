13e787707321abe6488c1bb98bf28f3a5763b6ce
Fabricio Augusto Oliveira Bastos <168025112+Gutobastos@users.noreply.github.com>
2025-04-15 16:30:21 -0300

    Update Principal.java

--- src/br/com/alura/minhasmusicas/principal/Principal.java
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -3,12 +3,17 @@ package br.com.alura.minhasmusicas.principal;
 import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
 import br.com.alura.minhasmusicas.modelos.Musica;
 import br.com.alura.minhasmusicas.modelos.Podcast;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner formulario = new Scanner(System.in);
         Musica minhaMusica = new Musica();
-        minhaMusica.setTitulo("Forever");
-        minhaMusica.setCantor("Kiss");
+        System.out.println("MÚSICA :");
+        System.out.println("Informe o título da música:");
+        minhaMusica.setTitulo(formulario.nextLine());
+        System.out.println("Informe o nome do artista:");
+        minhaMusica.setCantor(formulario.nextLine());
 
         for (int i = 0; i < 1000; i++) {
             minhaMusica.reproduz();
@@ -19,8 +24,11 @@ public class Principal {
         }
 
         Podcast meuPodcast = new Podcast();
-        meuPodcast.setTitulo("BolhaDev");
-        meuPodcast.setApresentador("Marcus Mendes");
+        System.out.println("PODCAST :");
+        System.out.println("Informe o nome do título:");
+        meuPodcast.setTitulo(formulario.nextLine());
+        System.out.println("Informe o nome do artista:");
+        meuPodcast.setApresentador(formulario.nextLine());
 
         for (int i = 0; i < 5000; i++) {
             meuPodcast.reproduz();
@@ -31,7 +39,11 @@ public class Principal {
         }
 
         MinhasPreferidas preferidas = new MinhasPreferidas();
-        preferidas.inclui(meuPodcast);
+        System.out.println("**********************************************************************");
         preferidas.inclui(minhaMusica);
+        System.out.println("**********************************************************************");
+        preferidas.inclui(meuPodcast);
+        System.out.println("**********************************************************************");
+
     }
 }
